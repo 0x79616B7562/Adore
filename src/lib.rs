d3625417c115938ec1ed1d050ff9f44b7eb4b038
0x79616B7562 <jakub.giesler.1@gmail.com>
2024-03-01 01:07:01 +0100

    reserving name on crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,44 @@
-include!("macros.rs");
-
-pub use wrld::Desc;
-
-pub mod shader {
-    #[allow(unused_imports)]
-    pub(crate) use include_wgsl_oil::include_wgsl_oil_crated as load_internal;
-    pub use include_wgsl_oil::include_wgsl_oil_ember as load;
-}
-
-pub use cast::cast;
-pub use glam;
-pub use log;
-pub use naga;
-
+// include!("macros.rs");
 //
-
-mod adore;
-mod api;
-mod assets;
-mod gfx;
-pub mod logger;
-mod math;
-mod time;
-mod traits;
-mod types;
-mod window;
-
-pub use adore::*;
-pub use api::*;
-pub use assets::*;
-pub use gfx::*;
-pub use math::*;
-pub use time::*;
-pub use traits::*;
-pub use types::*;
-pub use window::{
-    KeyCode,
-    MouseButton,
-    WindowConfig,
-};
+// pub use wrld::Desc;
+//
+// pub mod shader {
+//     #[allow(unused_imports)]
+//     pub(crate) use include_wgsl_oil::include_wgsl_oil_crated as load_internal;
+//     pub use include_wgsl_oil::include_wgsl_oil_ember as load;
+// }
+//
+// pub use cast::cast;
+// pub use glam;
+// pub use log;
+// pub use naga;
+//
+// //
+//
+// mod adore;
+// mod api;
+// mod assets;
+// mod gfx;
+// pub mod logger;
+// mod math;
+// mod time;
+// mod traits;
+// mod types;
+// mod window;
+//
+// pub use adore::*;
+// pub use api::*;
+// pub use assets::*;
+// pub use gfx::*;
+// pub use math::*;
+// pub use time::*;
+// pub use traits::*;
+// pub use types::*;
+// pub use window::{
+//     KeyCode,
+//     MouseButton,
+//     WindowConfig,
+// };
 
 //     ,'``.._   ,'``.
 //     :,--._:)\,:,._,.:       All Glory to
